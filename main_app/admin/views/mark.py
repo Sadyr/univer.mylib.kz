8a982865753442af6a03addc3571b164ae25543e
sadyr_hp <sadyr0012@gmail.com>
2023-06-13 12:46:06 +0600

    created mudule mark

--- main_app/admin/views/mark.py
+++ main_app/admin/views/mark.py
@@ -74,6 +74,42 @@ def add_mark(group_id,student_id, semester_id,course_id, teacher_course_id,teach
     else:
         return render_template('admin/mark/add_mark.html',group_id=group_id,student_id=student_id, semester_id=semester_id,course=course, teacher_course_id=teacher_course_id,teacher_id=teacher_id, control_points=control_points)
 
+
+@bp.route('/delete_mark/<int:mark_id>$<int:student_id>$<int:group_id>')
+@routes.login_required
+def delete_mark(mark_id, student_id, group_id):
+    with g.database as db:
+        mark = get_mark(mark_id)
+        db.delete(mark)
+        db.commit()
+    return redirect(url_for('admin.show_marks', student_id=student_id, group_id=group_id))
+
+def get_mark(mark_id):
+    with g.database  as db:
+            stmt = Select(Mark).filter(Mark.id == mark_id)
+            try:
+                mark = db.scalars(stmt).one()
+            except NoResultFound:
+                mark = 0
+    return mark
+
+@bp.route('/update_mark/<int:mark_id>$<int:student_id>$<int:group_id>', methods=('GET', 'POST'))
+@routes.login_required
+def update_mark(mark_id, group_id, student_id):
+    if request.method == 'POST':
+        with  g.database as db:
+            mark = db.query(Mark).filter(Mark.id==mark_id).first()
+            mark.mark=request.form['mark']
+            mark.control_point_id=request.form['control_point_id']
+            db.commit()
+            return redirect(url_for('admin.show_marks',group_id=group_id, student_id=student_id ))
+    else:
+        mark = get_mark(mark_id)
+        control_points = get_control_point()
+        return render_template('admin/mark/update_mark.html',control_points=control_points ,mark=mark )
+
+
+
 def get_course(course_id):
     with g.database  as db:
         stmt = Select(Course).filter(Course.id == course_id)
