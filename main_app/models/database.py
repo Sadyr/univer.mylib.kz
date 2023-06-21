62831b04827903f958eff025787d1061fa63f4f7
sadyr_hp <sadyr0012@gmail.com>
2023-06-21 15:37:31 +0600

    update

--- main_app/models/database.py
+++ main_app/models/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'postgresql+psycopg2://kaznuuser:Nimeria_1227@localhost/kaznudb'
-#DATABASE_URL = 'postgresql+psycopg2://kaznuuser:Nimeria_1227@185.4.180.217/kaznudb'
+DATABASE_URL = 'postgresql+psycopg2://kaznuuser:mk gkmt gkjt@localhost/kaznudb'
+#DATABASE_URL = 'postgresql+psycopg2://kaznuuser:komtkgm@185.4.180.217/kaznudb'
 
 engine = create_engine(DATABASE_URL, echo=True)
 Session = sessionmaker(bind=engine)
